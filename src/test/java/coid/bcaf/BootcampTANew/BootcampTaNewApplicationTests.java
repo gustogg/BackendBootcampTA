cc71e7a78c56120c14015f762501a873e1268597
Gusto Ginting <56625312+gustogg@users.noreply.github.com>
2024-11-13 17:20:03 +0700

    Initial Commit

---
+++ src/test/java/coid/bcaf/BootcampTANew/BootcampTaNewApplicationTests.java
@@ -0,0 +1,13 @@
+package coid.bcaf.BootcampTANew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampTaNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
